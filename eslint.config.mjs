52b934cf3af607da1cc8e5ff493390c87be2d72f
Bishal Kumar Sah <161715343+Sahbishal@users.noreply.github.com>
2025-12-28 17:19:35 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,8 @@ import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
+
+  
   ...nextTs,
   // Override default ignores of eslint-config-next.
   globalIgnores([
